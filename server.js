af8dbafb4918a535beb755d5255f4c78976fc383
*abrahamnyuiadzi* <abrahamnyuiadzi>
2024-09-22 18:29:27 +0000

    connect to mongo db

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express= require('express');
 const app = express();
+const mongodb= require('./data/database');
 
-
-app.use('/',require('./routes'));
 
 const port = process.env.PORT || 3000;
 
-app.listen(port,()=>{console.log(`Running on port  ${port}`)});
+app.use('/',require('./routes'));
+
+mongodb.initDb((err)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        app.listen(port,()=>{console.log(`Database is listining and node Running on port  ${port}`)});
+    }
+
+});
+
+
+
